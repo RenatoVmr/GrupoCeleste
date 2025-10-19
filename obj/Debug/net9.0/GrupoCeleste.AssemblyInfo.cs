ce2350ea621e70e4dcada27d5f639a4e49b303b8
lordedu99 <zangetsu45533@gmail.com>
2025-10-18 19:29:47 -0500

    HU-09: Implementar catálogo de películas

--- obj/Debug/net9.0/GrupoCeleste.AssemblyInfo.cs
+++ obj/Debug/net9.0/GrupoCeleste.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrupoCeleste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0694c13b6987332945f288a27040fca5b2502c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrupoCeleste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrupoCeleste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
