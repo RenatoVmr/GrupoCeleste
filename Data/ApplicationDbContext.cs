18b47ce6f64bef5f1688ba29888ba931ae9d1717
Gregorio Rojas <gregorio_rojas@usmp.pe>
2025-10-23 22:06:39 -0500

    feat: Configurar tabla Mensajes en base de datos

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,9 +12,28 @@ public class ApplicationDbContext : IdentityDbContext<Usuario>
     }
 
     public DbSet<Pelicula> Peliculas { get; set; }
+    public DbSet<Mensaje> Mensajes { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+        
+        // Configuración para la tabla Mensajes
+        builder.Entity<Mensaje>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Nombre).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.Email).IsRequired().HasMaxLength(150);
+            entity.Property(e => e.Asunto).IsRequired().HasMaxLength(200);
+            entity.Property(e => e.MensajeTexto).IsRequired().HasMaxLength(1000);
+            entity.Property(e => e.Fecha).IsRequired();
+            entity.Property(e => e.Telefono).HasMaxLength(20);
+            entity.Property(e => e.Leido).HasDefaultValue(false);
+            
+            // Índices para mejorar consultas
+            entity.HasIndex(e => e.Fecha);
+            entity.HasIndex(e => e.Leido);
+            entity.HasIndex(e => e.Email);
+        });
     }
 }
